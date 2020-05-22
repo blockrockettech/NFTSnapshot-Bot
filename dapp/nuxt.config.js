ede81f1049de674a7d40ca8b375785bb0a999957
James Morgan <james.morgan.e@googlemail.com>
2020-05-22 16:05:55 +0100

    setting up deployment, env vars and remving old webapp

--- dapp/nuxt.config.js
+++ dapp/nuxt.config.js
@@ -1,70 +1,75 @@
+require('./config');
+
 module.exports = {
   mode: 'universal',
+  server: {
+    port: process.env.PORT
+  },
   /*
-  ** Headers of the page
-  */
+   * Headers of the page
+   */
   head: {
     title: 'NFT Snapshot dapp',
     meta: [
       {
-        charset: "utf-8"
+        charset: 'utf-8'
       },
       {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1, shrink-to-fit=no"
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, shrink-to-fit=no'
       },
       {
-        name: "keywords",
-        content: "erc721, ethereum, blockchain, cryptoart"
+        name: 'keywords',
+        content: 'erc721, ethereum, blockchain, cryptoart'
       },
       {
         hid: 'title',
-        name: "title",
-        content: "NFT Snapshot"
+        name: 'title',
+        content: 'NFT Snapshot'
       },
       {
         hid: 'description',
-        name: "description",
-        content: "NFT Snapshot"
+        name: 'description',
+        content: 'NFT Snapshot'
       },
       {
         hid: 'subject',
-        name: "subject",
-        content: "NFT Snapshot"
+        name: 'subject',
+        content: 'NFT Snapshot'
       },
       {
-        name: "robots",
-        content: "index,follow"
+        name: 'robots',
+        content: 'index,follow'
       },
       {
         hid: 'og:title',
-        property: "og:title",
-        content: "NFT Snapshot"
+        property: 'og:title',
+        content: 'NFT Snapshot'
       },
       {
         hid: 'og:description',
-        property: "og:description",
-        content: "NFT Snapshot"
+        property: 'og:description',
+        content: 'NFT Snapshot'
       },
       {
         hid: 'og:url',
-        property: "og:url",
-        content: "http://example.com"
+        property: 'og:url',
+        content: 'http://example.com'
       },
       {
         hid: 'twitter:card',
-        property: "twitter:card",
-        content: "NFT Snapshot"
+        property: 'twitter:card',
+        content: 'NFT Snapshot'
       },
       {
         hid: 'twitter:description',
-        property: "twitter:description",
-        content: "NFT Snapshot"
+        property: 'twitter:description',
+        content: 'NFT Snapshot'
       },
       {
         hid: 'twitter:title',
-        property: "twitter:title",
-        content: "NFT Snapshot"
+        property: 'twitter:title',
+        content: 'NFT Snapshot'
       }
     ],
     link: [
